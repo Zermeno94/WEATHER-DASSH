fecfabfa432d0c93d7bff7baa8fdc2a5cf5475da
MIRANDA <miranda.zermeno4@gmail.com>
2021-09-15 00:00:19 -0500

    comments

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+// js logic breakdown: 
+
+//function initPage{}(declaring)
+
+// variables here 
+
+//console.log 
+
+//
+
+// apikey, need to get apikey
+
+//saved city name, get a current condition and pull request from weather api
+
+//link up apikey here 
+
+// figure out the rest 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Need to do:
+//THEN I am presented with current and future conditions for that city and that city is added to the search history
+//WHEN I view current weather conditions for that city
+//THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+//WHEN I view the UV index
+//THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+//WHEN I view future weather conditions for that city
+//THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+//WHEN I click on a city in the search history
+//THEN I am again presented with current and future conditions for that city//
