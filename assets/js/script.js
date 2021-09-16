981bc6c7fb02778df548b85df0bddd63df2aee4b
MIRANDA <miranda.zermeno4@gmail.com>
2021-09-15 22:40:41 -0500

    added more to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,36 @@
-// js logic breakdown: 
+// API Key
+var apiKey = "c835f5e0d135d1c788e373d4a940d4e0";
+var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=c835f5e0d135d1c788e373d4a940d4e0`
 
-//function initPage{}(declaring)
+// APIUrl: https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=c835f5e0d135d1c788e373d4a940d4e0
 
-// variables here 
+//reference OpenWeather: https://openweathermap.org/api/one-call-api#how
 
-//console.log 
+var searchButton = $(".searchButton");
 
-//
+//Forloop for persisting the data onto HMTL page
+for (var i = 0; i < localStorage.length; i++) {
 
-// apikey, need to get apikey
+    var city = localStorage.getItem(i);
+    // console.log(localStorage.getItem("City"));
+    var cityName = $(".list-group").addClass("list-group-item");
 
-//saved city name, get a current condition and pull request from weather api
-
-//link up apikey here 
-
-// figure out the rest 
-
-
-
-
-
-
-
-
-
-
-
-
+    cityName.append("<li>" + city + "</li>");
+}
 
 // Need to do:
 //THEN I am presented with current and future conditions for that city and that city is added to the search history
+
 //WHEN I view current weather conditions for that city
+
 //THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+
 //WHEN I view the UV index
+
 //THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
 //WHEN I view future weather conditions for that city
+
 //THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 //WHEN I click on a city in the search history
 //THEN I am again presented with current and future conditions for that city//
