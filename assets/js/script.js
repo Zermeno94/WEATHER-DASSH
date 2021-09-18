4d0d05b8d7643707b6c98001c3cee64da962fbe9
MIRANDA <miranda.zermeno4@gmail.com>
2021-09-17 22:14:44 -0500

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,10 @@
-var city="";
-//declares a variable to store the searched city 
-
-//variables 
-var searchLocation = $("#search-location");
-var searchButton = $("#search-button");
-var clearHistory = $("#clear-history");
-var currentLocation = $("#current-location");
-var currentTemperature = $("#temperature");
-var currentHumdity = $("#current-humdity");
-var currentWindSpeed = $("#current-windspeed");
-var currentUvIndex = $("#uv-index");
-var sLocation=[];
-
-
-function find(c){ 
-    for (var i=0; i<sLocation.length; i++){
-        if(c.toUppercase()===sLocation[i]) //entries entered in the storage
-    {
-        return -1;
-    }
-}
-
 // API Key
-var apiKey = "c835f5e0d135d1c788e373d4a940d4e0";
+var apiKey = "fde9c903c898c317388e65c683b198c3";
+
+var searchButton =$(".searchButton");
 
 //Fetching api data 
-//api url: https://api.openweathermap.org/data/2.5/weather?q=&appid=c835f5e0d135d1c788e373d4a940d4e0
+//api url: https://api.openweathermap.org/data/2.5/weather?q=&appid=fde9c903c898c317388e65c683b198c3
 function currentWeather() {
 
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + inputBoxEl.value + "&appid=" + apiKey)
@@ -36,9 +15,8 @@ function currentWeather() {
         })
 //reference OpenWeather: https://openweathermap.org/api/one-call-api#how
 
-var searchButton = $(".searchButton");
-
 //Forloop for persisting the data onto HMTL page
+
 for (var i = 0; i < localStorage.length; i++) {
 
     var city = localStorage.getItem(i);
