482d95f7a1f7e5b2fc9c9e96ebad3d629c50bcf6
MIRANDA <miranda.zermeno4@gmail.com>
2021-09-17 19:05:47 -0500

    added more js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,42 @@
+var city="";
+//declares a variable to store the searched city 
+
+//variables 
+var searchLocation = $("#search-location");
+var searchButton = $("#search-button");
+var clearHistory = $("#clear-history");
+var currentLocation = $("#current-location");
+var currentTemperature = $("#temperature");
+var currentHumdity = $("#current-humdity");
+var currentWindSpeed = $("#current-windspeed");
+var currentUvIndex = $("#uv-index");
+var sLocation=[];
+
+
+function find(c){ 
+    for (var i=0; i<sLocation.length; i++){
+        if(c.toUppercase()===sLocation[i]) //entries entered in the storage
+    {
+        return -1;
+    }
+}
+
 // API Key
 var apiKey = "c835f5e0d135d1c788e373d4a940d4e0";
-var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=c835f5e0d135d1c788e373d4a940d4e0`
 
-// APIUrl: https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=c835f5e0d135d1c788e373d4a940d4e0
+//Fetching api data 
+//api url: https://api.openweathermap.org/data/2.5/weather?q=&appid=c835f5e0d135d1c788e373d4a940d4e0
+function currentWeather() {
+
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + inputBoxEl.value + "&appid=" + apiKey)
+        .then(function (response) {
+            if(response.ok) {
+            return response.json()
+            }
+        })
+
+
+
 
 //reference OpenWeather: https://openweathermap.org/api/one-call-api#how
 
@@ -35,4 +69,4 @@ for (var i = 0; i < localStorage.length; i++) {
 
 //THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 //WHEN I click on a city in the search history
-//THEN I am again presented with current and future conditions for that city//
+//THEN I am again presented with current and future conditions for that city/
